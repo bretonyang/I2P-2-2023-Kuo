6b4d5c697e5608edb12c094c432d866cc40761c6
bretonyang <breton0429@gmail.com>
2023-03-20 02:04:43 +0800

    HW2: finished

--- HW/HW2/13436_Remove_the_leaves/main.c
+++ HW/HW2/13436_Remove_the_leaves/main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct node {
     int val;
     struct node* next;
 } Node;
 
-/// Note that vertices are 1-based
 Node* adj[20005];
 int deg[20005];
 int removed[20005];
-int queue[20005];
-// total: ~80000 * 4 = 320,000
-
+int num_vertices;
+int deg1_nodes[20005];
+int initialized[20005];
+int init_cnt = 1, T;
 
 Node* create_node(int val, Node* next) {
     Node* new_node = malloc(sizeof(Node));
@@ -22,142 +21,92 @@ Node* create_node(int val, Node* next) {
     return new_node;
 }
 
-
-void init_values(int N) {
-    for (int i = 1; i <= N; i++) {
-        removed[i] = 0;
-        deg[i] = 0;
-        adj[i] = create_node(i, NULL);
-    }
-}
-
-
-void create_adj(int N) {
+void build_graph(int N) {
     int v, u;
-    for (int i = 0; i < N - 1; i++) {
+    for (int i = 1; i <= N - 1; i++) {
         scanf("%d%d", &v, &u);
+        if (initialized[v] != init_cnt) {
+            adj[v] = NULL;
+            deg[v] = 0;
+            initialized[v]++;
+        }
+        if (initialized[u] != init_cnt) {
+            adj[u] = NULL;
+            deg[u] = 0;
+            initialized[u]++;
+        }
+        Node* v_node = create_node(v, adj[u]);
+        Node* u_node = create_node(u, adj[v]);
+        adj[v] = u_node;
+        adj[u]= v_node;
         deg[v]++;
         deg[u]++;
-        Node* v_node = create_node(u, adj[v]->next);
-        Node* u_node = create_node(v, adj[u]->next);
-        adj[v]->next = v_node;
-        adj[u]->next = u_node;
+        removed[i] = 0;
     }
+    removed[N] = 0;
+    init_cnt++;
 }
 
-
-void free_adj(int N) {
+void free_graph(int N) {
     for (int i = 1; i <= N; i++) {
         Node* tmp = adj[i];
-        while (tmp != NULL && tmp->next != NULL) {
+        while (tmp != NULL) {
             Node* next = tmp->next;
             free(tmp);
             tmp = next;
         }
-        free(tmp);
     }
 }
 
+void solve(int N, int K) {
+    int deg1_cnt = 0, tmp_deg1_cnt = 0;
+    for (int i = 1; i <= N; i++) {
+        if (deg[i] == 1) {
+            deg1_nodes[deg1_cnt++] = i;
+            if (K > 0) num_vertices--;
+        }
+    }
 
-int solve(int N, int K) {
-    int num_vertices = N;
-    while (K-- && num_vertices > 0) {
-        int queue_idx = 0;
-        for (int v = 1; v <= N; v++) {
-            if (!removed[v] && deg[v] <= 1) {
-                removed[v] = 1;
-                num_vertices--;
-                Node* tmp = adj[v]->next;
-                while (tmp) {
-                    if (!removed[tmp->val]) {
-                        queue[queue_idx++] = tmp->val;
+    K--;
+    while (K-- > 0 && num_vertices > 0) {
+        if (num_vertices <= 2) {
+            num_vertices = 0;
+            break;
+        }
+        tmp_deg1_cnt = 0;
+        for (int i = 0; i < deg1_cnt; i++) {
+            int cur_node = deg1_nodes[i];
+            Node* tmp = adj[cur_node];
+
+            while (tmp != NULL) {
+                if (!removed[tmp->val]) {
+                    deg[tmp->val]--;
+                    if (deg[tmp->val] == 1) {
+                        deg1_nodes[tmp_deg1_cnt++] = tmp->val;
+                        num_vertices--;
+                        removed[tmp->val] = 1;
                     }
-                    tmp = tmp->next;
                 }
+                tmp = tmp->next;
             }
         }
-        for (int w = 0; w < queue_idx; w++) {
-            deg[queue[w]]--;
-        }
+        deg1_cnt = tmp_deg1_cnt;
     }
-
-    return num_vertices;
 }
-
-
-/// For Debugging
-void print_adj(int N) {
-    printf("Adjacency List:\n");
-    for (int i = 1; i <= N; i++) {
-        Node* tmp = adj[i]->next;
-        printf("%d: ", i);
-        while (tmp != NULL) {
-            printf("%d ", tmp->val);
-            tmp = tmp->next;
-        }
-        printf("\n");
-    }
-    printf("Adjacency List finished\n");
-}
-void print_info(int N) {
-    printf("Removed:\n");
-    for (int i = 1; i <= N; i++) {
-        printf("%d ", removed[i]);
-    }
-    printf("\nRemoved Finished\n");
-    printf("Degree:\n");
-    for (int i = 1; i <= N; i++) {
-        printf("%d ", deg[i]);
-    }
-    printf("\nDegree Finished\n");
-}
-
 
 int main()
 {
-    int T;
     scanf("%d", &T);
-
     while (T--) {
         int N, K;
         scanf("%d%d", &N, &K);
 
-        init_values(N);
-        create_adj(N);
+        build_graph(N);
+        num_vertices = N;
+        solve(N, K);
+        printf("%d\n", num_vertices);
 
-//        print_adj(N); /// For Debugging
-
-        // Put this here so that we still read in input
-        if (K == N) {
-            printf("0\n");
-            continue;
-        }
-        int num_vertices_left = solve(N, K);
-
-//        print_info(N); /// For Debugging
-
-        printf("%d\n", num_vertices_left);
-        free_adj(N);
+        free_graph(N);
     }
-
     return 0;
 }
-
-
-/**
-
-numVertices = N, list_of_nodes = [...]
-for k in range(K):
-    queue = Queue()
-    for v in list_of_nodes:
-        if not removed[v] and deg[v] <= 1:
-            numVertices--
-            removed[v] = true
-            for w in list_of_nodes[v].adj:
-                if not removed[w]:
-                    queue.enqueue(w)
-        list_of_nodes.remove(v)
-    for v in queue:
-        deg[V]--
-
-*/
