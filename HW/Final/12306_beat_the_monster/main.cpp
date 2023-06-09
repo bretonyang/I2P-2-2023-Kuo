1bc56a52ee36c1cd273af480ba7ac4f82cbdf11e
bretonyang <breton0429@gmail.com>
2023-06-10 02:55:14 +0800

    fixed bug in checking healing condition order

--- HW/Final/12306_beat_the_monster/main.cpp
+++ HW/Final/12306_beat_the_monster/main.cpp
@@ -2,38 +2,50 @@
 
 using namespace std;
 
+//struct ArrayHasher {
+//    int operator()(const array<int, 3> &V) const {
+//        int hash = V.size();
+//        for(auto &i : V) {
+//            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+//        }
+//        return hash;
+//    }
+//};
+
 class Game {
     using State = array<int, 3>; // level, HP, monster_HP
 private:
     int maxLevel;
     int maxHP;
     int monsterDamage;
-    vector<vector<int>> table; // vector of "damage" and "Heal"
+    vector<vector<int>> table; // vector of (damage, heal)
     int distTo[16][301][401] = {};
     bool visited[16][301][401] = {};
     State initState;
 
     vector<State> neighbors(State s) {
-        // since player starts first, so we should check playerHP in the following
-        s[1] -= monsterDamage;
+        // CAREFUL: player starts first, then monster attack!!!
         vector<State> res;
 
         // ATTACK
         int monsterHP = max(0, s[2] - table[s[0]][0]);
         if (monsterHP <= 0)
-            res.push_back(State{s[0], s[1] + monsterDamage, monsterHP}); // our attack kills monster first
-        else if (s[1] > 0)
-            res.push_back(State{s[0], s[1], monsterHP});
+            res.push_back(State{s[0], s[1], monsterHP}); // our attack kills monster first
+        else if (s[1] - monsterDamage > 0)
+            res.push_back(State{s[0], s[1] - monsterDamage, monsterHP});
 
-        // HEAL
-        int newHP = s[1] + table[s[0]][1];
-        if (newHP > 0 && newHP <= maxHP)
-            res.push_back(State{s[0], newHP, s[2]});
+        // HEAL (We NEED to calculate healed HP first before attacked by monster)
+        // wrong method: I originally calculated player's HP after attacked by monster, then check healing
+        int newHP = min(s[1] + table[s[0]][1], maxHP);
+        if (newHP - monsterDamage > 0)
+            res.push_back(State{s[0], newHP - monsterDamage, s[2]});
 
-        // LEVEL-UP
-        if (s[1] > 0) {
-            if (s[0] + 1 <= maxLevel)
-                res.push_back(State{s[0] + 1, s[1], s[2]});
+        // LEVEL-UP (either way are fine)
+        if (s[1] - monsterDamage > 0) {
+            int newLevel = min(s[0] + 1, maxLevel);
+            res.push_back(State{newLevel, s[1] - monsterDamage, s[2]});
+//            if (s[0] + 1 <= maxLevel)
+//                res.push_back(State{s[0] + 1, s[1] - monsterDamage, s[2]});
         }
 
         return res;
@@ -44,6 +56,8 @@ private:
     }
 
     int bfs(State s) {
+        /// although using unordred_map is also possible, but it's a lot slower (800ms vs. 80ms)
+        /// It actually makes sense, since HashMaps are "amortized" contant vs. "always" constant by arrays
 //        unordered_map<State, int, ArrayHasher> distTo;
 //        unordered_map<State, bool, ArrayHasher> visited;
         queue<State> q;
@@ -80,6 +94,9 @@ public:
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int L, HP, MHP, MDMG;
     cin >> L >> HP >> MHP >> MDMG;
     vector<vector<int>> table(L, vector<int>(2));
